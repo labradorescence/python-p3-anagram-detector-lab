b993da35cd3e382f90fffe5467b2997309418dad
labradorescence <orangenewyorkcreativeagency@gmail.com>
2022-12-26 16:52:19 -0500

    loop, sorted, append

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.sorted_letter = sorted([ each_let for each_let in word])
+
+        #print(self.sorted_letter)
+
+    def match(self, list_arr):
+        result = []
+        for each_word in list_arr:
+            #print(each_word)
+            if(sorted([each_let for each_let in each_word]) == self.sorted_letter):
+                result.append(each_word)
+        
+        print(result)
+        return result
+                
+
+
+new_word = Anagram("enlist")
+new_word.match(["listen", "poison", "hello"])
